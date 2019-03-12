46ac90d26dc7e69a09f601a4bfc0b2408b7ce719
ABradRoberts <bradr555@gmail.com>
2019-03-11 21:13:52 -0500

    Add .gitignore, Update program to read and write global matrix.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <fstream>
+#include <array>
 using namespace std;
 
-int main(){
-	cout << "Init." << endl;
+void readMatrix();
+void printMatrix();
+
+int matrix[4][4];
+
+int main()
+{
+	readMatrix();
+
+	cout << "INPUT ARRAY:" << endl;
+	cout << "------------" << endl;
+
+	printMatrix();
+
 	return 0;
+}
+
+void printMatrix()
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			cout << matrix[i][j];
+			cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+void readMatrix()
+{
+	ifstream file("input.txt");
+
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			file >> matrix[i][j];
+		}
+	}
 }
